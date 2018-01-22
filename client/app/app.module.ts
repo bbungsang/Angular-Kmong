9dc7e23ffb2d1e20e5978f1ccf6d38e9d56c6a23
bbungsang <821heejin@naver.com>
2018-01-22 14:42:05 +0900

    login 앱 생성, 라우터 설정

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +14,11 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
